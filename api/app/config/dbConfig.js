b3cf1adadf655ed2defaff3d97cf7e133fb017f1
Fivi Melinda <fivimelinda@gmail.com>
2021-01-27 15:41:06 +0700

    update dbconfig

--- api/app/config/dbConfig.js
+++ api/app/config/dbConfig.js
@@ -2,8 +2,8 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres", //MASUKKAN NAMA USER POSTGRESQL
   host: "localhost", //MASUKKAN HOST
-  database: "last", //MASUKKAN NAMA DATABASE
-  password: "197399", //MASUKKAN PASSWORD
+  database: "gisdb", //MASUKKAN NAMA DATABASE
+  password: "password", //MASUKKAN PASSWORD
   port: 5432, //MASUKKAN PORT YANG DIGUNAKAN DATABASE
 });
 
